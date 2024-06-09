93627faa458423125947eb4f362040316e25661a
Tomaz Zlender <tomaz@84codes.com>
2024-06-09 03:41:41 +0200

    Generate a gem `snapshot_ui`
    ```
    bundle gem snapshot_ui --no-exe --no-ext --mit --no-coc --test=minitest --linter=standard --ci=github
    ```

---
+++ lib/snapshot_ui.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "snapshot_ui/version"
+
+module SnapshotUi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_snapshot_ui.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSnapshotUi < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::SnapshotUi::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
