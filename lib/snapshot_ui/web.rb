bc25fe68ddef97401a12900bcd0b8a65dce9f773
Tomaz Zlender <tomaz@84codes.com>
2024-06-09 06:42:49 +0200

    Add a view to list snapshots

--- lib/snapshot_ui/web.rb
+++ lib/snapshot_ui/web.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
+require "erb"
+
 module SnapshotUI
   class Web
     def call(_env)
-      [200, {"content-type" => "text/html"}, ["A list of snapshots"]]
+      render "snapshots/index", status: 200
+    end
+
+    private
+
+    def render(template, status:)
+      @content = render_template(template)
+      body = render_template("layout")
+      headers = {"content-type" => "text/html; charset=utf-8"}
+
+      [status, headers, [body]]
+    end
+
+    def render_template(template)
+      template = File.read("#{File.dirname(__FILE__)}/web/views/#{template}.html.erb")
+      erb = ERB.new(template)
+      erb.result(binding)
     end
   end
 end
--- test/snapshot_ui/web_test.rb
+++ test/snapshot_ui/web_test.rb
@@ -14,6 +14,7 @@ class SnapshotUI::WebTest < Minitest::Spec
 
   it "renders a list of snapshots" do
     get "/ui/snapshots"
-    _(last_response.body).must_match("A list of snapshots")
+    _(last_response.body).must_match("Snapshots")
+    _(last_response.body).must_match("A list of snapshots...")
   end
 end
