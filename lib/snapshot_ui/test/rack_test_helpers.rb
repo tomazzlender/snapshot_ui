16aadb82966be6525d5b07292db2f6d5a8470258
Tomaz Zlender <tomaz@84codes.com>
2024-06-10 01:50:34 +0200

    Persist a response body to a file named after a test case

--- lib/snapshot_ui/test/rack_test_helpers.rb
+++ lib/snapshot_ui/test/rack_test_helpers.rb
@@ -2,8 +2,9 @@ module SnapshotUI
   module Test
     module RackTestHelpers
       def take_snapshot(snapshotee)
+        test_case_name = @NAME
         Pathname.new(SnapshotUI.configuration.storage_directory).mkpath
-        file_path = File.join(SnapshotUI.configuration.storage_directory, "sample_test_case.html")
+        file_path = File.join(SnapshotUI.configuration.storage_directory, "#{test_case_name}.html")
         File.write(file_path, snapshotee.body)
       end
     end
