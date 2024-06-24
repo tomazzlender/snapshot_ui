0beb1364c3b17a27dd208f9acd35023657382353
Tomaz Zlender <tomaz@84codes.com>
2024-06-24 06:42:10 +0200

    Add snapshot UI plugin for Minitest
    - a command line option "--take-snapshots" which enables snapshot taking
    - before a test suit is run we want to ensure the folder `tmp/snapshot_ui/in_progress` is clean
    - after a test suit is complete we want to ensure the taken snapshots are published

---
+++ lib/minitest/snapshot_ui_plugin.rb
@@ -0,0 +1,28 @@
+require "snapshot_ui"
+require "minitest"
+
+module Minitest
+  class << self
+    def plugin_snapshot_ui_options(opts, _options)
+      opts.on "--take-snapshots", "Take UI snapshots" do
+        ENV["TAKE_SNAPSHOTS"] = "true"
+      end
+    end
+
+    def plugin_snapshot_ui_init(_options)
+      return unless SnapshotUI.snapshot_taking_enabled?
+
+      reporter << SnapshotUIReporter.new
+
+      SnapshotUI.clear_snapshots_in_progress
+    end
+  end
+
+  class SnapshotUIReporter < Reporter
+    def report
+      SnapshotUI.publish_snapshots_in_progress
+
+      io.print "\n\nUI snapshots are ready for review at #{SnapshotUI.configuration.web_url}"
+    end
+  end
+end
