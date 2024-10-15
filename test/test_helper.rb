8833eafa9fdcb003e1f37f1f2d13238ebb1c53d2
Tomaz Zlender <tomaz@84codes.com>
2024-10-15 06:23:23 +0200

    For better DX while testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,5 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "snapshot_ui"
 require "minitest/autorun"
+require "minitest/focus"
 require_relative "dummy/config/snapshot_ui_initializer"
